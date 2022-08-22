38d0195ea1663f5b81cc7f890b3e2916a564dd10
Cicadas3301 <59025608+Cicadas3301@users.noreply.github.com>
2022-08-22 13:29:38 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -73,7 +73,8 @@ $(document).ready(function() {
 	})
 
 	$('.calendar_menu-top').on('click', function () {
-		$(this).siblings('.calendar_menu-inputs').toggleClass('active');
+		$(this).siblings('.calendar_menu-inputs').slideToggle();
+		$(this).toggleClass('active');
 	})
 
 	$('.js-emailbutton').on('click', function () {
@@ -106,14 +107,21 @@ $(document).ready(function() {
 
 	$('#datepicker').datepicker({
 		inline: true,
-		//nextText: '&rarr;',
-		//prevText: '&larr;',
-		showOtherMonths: true,
-		//dateFormat: 'dd MM yy',
-		dayNamesMin: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
-		// showOn: "button",
-		//buttonImage: "img/calendar-blue.png",
-		//buttonImageOnly: true,
+		closeText: 'Закрыть',
+		prevText: 'Предыдущий',
+		nextText: 'Следующий',
+		currentText: 'Сегодня',
+		monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+		monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+		dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+		dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+		weekHeader: 'Не',
+		dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''
 	});
 
 	$('.games_top-select select').select2();
